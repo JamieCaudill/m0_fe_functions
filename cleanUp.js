560188deed69d07964fd6515ab0817323474db82
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-02 10:53:20 -0800

    Clean up average function

--- cleanUp.js
+++ cleanUp.js
@@ -8,24 +8,24 @@
 
 
 // EX 1:
-// function askForName() { 
-//   console.log("Hello, what is your name?"); 
-// }
+function askForName() { 
+  console.log("Hello, what is your name?"); 
+}
 
-// askForName();
+askForName();
 
-// Moved console.log to next line, added semi-colons and moved } to next line because that is the proper syntax.
+// Move console.log to next line, added semi-colons and moved } to next line because that is the proper syntax.
 
 // EX 2:
-// function addThreeNums(first, second, third) {
-//   var sum = first + second + third;
-//   return sum;
-// }
+function addThreeNums(first, second, third) {
+  var sum = first + second + third;
+  return sum;
+}
 
-// console.log(addThreeNums(1, 2, 3));
-// console.log(addThreeNums(4, 2, 7));
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
 
-// Indented lines 21 and 22 and added semicolons. Checked code with console.log.
+// Indent lines 21 and 22 and added semicolons. Check code with console.log.
 
 // // EX 3:
 function makeFreshPesto() {
@@ -38,13 +38,15 @@ function makeFreshPesto() {
 
 makeFreshPesto();
 
-// Changed 'func' to 'function.' Space before {. Moved } from line 36 to 37.
+// Change 'func' to 'function.' Space before {. Moved } from line 36 to 37.
 
 // //  EX 4:
-// function average(num1, num2) 
-//   {
-// var sum = num1 + num2;
-//     var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return avg
+}
 
-// return avg
-//   }
+console.log(average(4, 8));
+
+// Add semicolon line 49. Move { from line 45 to 44. Indent line 45. Delete extra indent line 46. Move return up a line and indent. move } up a line and delete extra indent. Check code with console.log.
