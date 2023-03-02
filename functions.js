8d197131967e850240dc078e599382b92f4498a9
Jamie Caudill <j.caudill7177@gmail.com>
2023-03-02 12:02:15 -0800

    Write square function

--- functions.js
+++ functions.js
@@ -28,10 +28,19 @@ function greetPerson(first, middle, last) {
 console.log(greetPerson("Gary", "Voldemort", "McGriddle"));
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
+
+function square(x) {
+    return Math.pow(x, 2);
+}
+
+console.log(square(4));
+
 // BONUS: Print a sentence that interpolates the return value of your square function.
 
-
+x = 10;
+console.log(`The square of ${x} is ${square(x)}`);
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
+
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
 
 // checkStock(4, "Coffee");
